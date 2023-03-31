4efdf06a06a2745b948b0406e7dc568d5eac865f
Ahmed babiker <you@example.com>
2023-03-31 00:05:19 -0400

    add new updates

--- job/models.py
+++ job/models.py
@@ -1,11 +1,16 @@
 from django.db import models
-
+# from django.contrib.gis.db import models
 # Create your models here.
 
 JOB_TYPE = {
     ('Full Time', 'Full Time'),
     ('Part Time', 'Part Time'),
 }
+
+
+def image_upload(instance, filename):
+    image_names, extension = filename.split(".")
+    return "jobs/%s/%s.%s" % (instance.id, instance.id, extension)
 
 
 class Job(models.Model):
@@ -17,6 +22,9 @@ class Job(models.Model):
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    email = models.EmailField(max_length=254)
+    # location = models.PointField()
+    image = models.ImageField(upload_to=image_upload)
 
     def __str__(self):
         return self.title
@@ -27,3 +35,11 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+# class Location(models.Model):
+#     name = models.CharField(max_length=255)
+#     point = models.PointField()
+
+#     def __str__(self):
+#         return self.name
--- project/settings.py
+++ project/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 'django.contrib.gis',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     # job
     'job',
 ]
@@ -118,7 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / "media"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
